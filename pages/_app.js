158361f0434f981c781288638772c529a6ce4f7b
nehalrauf199-sudO <raufh137@gmail.com>
2026-04-03 19:22:51 +0500

    Added Google Analytics with measurement ID

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
 import { Analytics } from '@vercel/analytics/react'
+import { GoogleAnalytics } from 'react-next-google-tools'
 
 export default function MyApp({ Component, pageProps }) {
     return (
         <>
-            <Component {...pageProps} />
+            {/* Google Analytics */}
+            <GoogleAnalytics id="G-D6H3SB370C" isNextJs={true} />
+
+            {/* Vercel Analytics */}
             <Analytics />
+
+            <Component {...pageProps} />
         </>
     )
 }
