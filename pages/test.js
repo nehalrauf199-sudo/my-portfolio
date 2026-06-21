0d94163d5389733e40c672a36e26252f3c3fd198
nehalrauf199-sudO <raufh137@gmail.com>
2026-06-21 13:44:33 +0500

    Add test page to check routing

---
+++ pages/test.js
@@ -0,0 +1,9 @@
+export default function Test() {
+    return (
+        <div style={{ padding: '100px 20px', textAlign: 'center' }}>
+            <h1>✅ Test Page Working!</h1>
+            <p>If you can see this, Next.js is working fine.</p>
+            <a href="/">Go Home</a>
+        </div>
+    );
+}
